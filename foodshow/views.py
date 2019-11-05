b49de2f801c663ef2927ce5a83ff698773767211
oz kilim <ozkilim@hotmail.co.uk>
2019-11-05 19:23:13 +0100

    no cam

--- foodshow/views.py
+++ foodshow/views.py
@@ -225,44 +225,44 @@ def eaten(request):
     return render(request, 'eaten.html', {"eaten_foods": eaten_foods})
 
 
-class VideoCamera(object):
-    def __init__(self):
-        # Using OpenCV to capture from device 0. If you have trouble capturing
-        # from a webcam, comment the line below out and use a video file
-        # instead.
-        self.video = cv2.VideoCapture(0)
-        # If you decide to use video.mp4, you must have this file in the folder
-        # as the main.py.
-        # self.video = cv2.VideoCapture('video.mp4')
-
-    def __del__(self):
-        self.video.release()
-
-    def get_frame(self):
-        success, image = self.video.read()
-        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
-        # so we must encode it into JPEG in order to correctly display the
-        # video stream.
-
-        # _, frame = cap.read()
-        decodedObjects = pyzbar.decode(image)
-        thedata = None
-        for obj in decodedObjects:
-            print("Data", obj.data)
-            # this is the print...
-            thedata = obj.data
-        ret, jpeg = cv2.imencode('.jpg', image)
-
-        return jpeg.tobytes(), thedata
-
-
-def gen(camera):
-    while True:
-        frame = camera.get_frame()[0]
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        if camera.get_frame()[1] is not None:
-            return render(request, "index.html")
+# class VideoCamera(object):
+#     def __init__(self):
+#         # Using OpenCV to capture from device 0. If you have trouble capturing
+#         # from a webcam, comment the line below out and use a video file
+#         # instead.
+#         self.video = cv2.VideoCapture(0)
+#         # If you decide to use video.mp4, you must have this file in the folder
+#         # as the main.py.
+#         # self.video = cv2.VideoCapture('video.mp4')
+#
+#     def __del__(self):
+#         self.video.release()
+#
+#     def get_frame(self):
+#         success, image = self.video.read()
+#         # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+#         # so we must encode it into JPEG in order to correctly display the
+#         # video stream.
+#
+#         # _, frame = cap.read()
+#         decodedObjects = pyzbar.decode(image)
+#         thedata = None
+#         for obj in decodedObjects:
+#             print("Data", obj.data)
+#             # this is the print...
+#             thedata = obj.data
+#         ret, jpeg = cv2.imencode('.jpg', image)
+#
+#         return jpeg.tobytes(), thedata
+#
+#
+# def gen(camera):
+#     while True:
+#         frame = camera.get_frame()[0]
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#         if camera.get_frame()[1] is not None:
+#             return render(request, "index.html")
 
 
 
