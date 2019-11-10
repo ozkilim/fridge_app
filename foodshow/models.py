6ec5dd544ca40f7a3755d75f5d3e4f40e43f8a21
oz kilim <ozkilim@hotmail.co.uk>
2019-11-10 15:22:05 +0100

    emails sent daily true for testing

--- foodshow/models.py
+++ foodshow/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    send_daily_emails = models.BooleanField(default=False)
+    send_daily_emails = models.BooleanField(default=True)
 
 
     def __str__(self):
